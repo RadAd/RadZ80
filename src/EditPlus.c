b6a4562fe148ff90f39bcf834ee0f7d84c088ef1
RadAd <adamgates84+github@gmail.com>
2025-07-16 09:27:40 +1000

    Fix for ascii

--- src/EditPlus.c
+++ src/EditPlus.c
@@ -2,6 +2,7 @@
 
 #include <ctype.h>
 #include <commctrl.h>
+#include <tchar.h>
 
 LRESULT CALLBACK EditHexChar(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData)
 {
@@ -10,7 +11,7 @@ LRESULT CALLBACK EditHexChar(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
     case WM_CHAR:
     {
         const TCHAR c = (TCHAR) wParam;
-        if (iswprint(c) && !iswxdigit(c))
+        if (_istprint(c) && !_istxdigit(c))
             return MessageBeep(0), 0;
         else
             return DefSubclassProc(hWnd, uMsg, wParam, lParam);
